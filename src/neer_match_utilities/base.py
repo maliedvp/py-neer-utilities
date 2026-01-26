64a6ef5c561cc3ae97fdbffc4e07ebb026b36504
Marius Liebald <maliedvp@gmail.com>
2026-01-26 18:56:42 +0100

    allow loaded similarity function to be passed to Prepare

--- src/neer_match_utilities/base.py
+++ src/neer_match_utilities/base.py
@@ -1,8 +1,13 @@
 from pandas import DataFrame
+from neer_match.similarity_map import SimilarityMap
 
 class SuperClass:
-    def __init__(self, similarity_map: dict, df_left: DataFrame = DataFrame(), df_right: DataFrame = DataFrame(), id_left: str = "id", id_right: str = "id"):
-        self.similarity_map = similarity_map
+    def __init__(self, similarity_map: dict | SimilarityMap, df_left: DataFrame = DataFrame(), df_right: DataFrame = DataFrame(), id_left: str = "id", id_right: str = "id"):
+        # Convert SimilarityMap to dict if needed
+        if isinstance(similarity_map, SimilarityMap):
+            self.similarity_map = similarity_map.instructions
+        else:
+            self.similarity_map = similarity_map
         self.df_left = df_left
         self.df_right = df_right
         self.id_left = id_left
